f55ed3729f557dde16a6a7284c417a2bf13b85d2
fayuan-pineapplepie <fayuan@asbarcelona.com>
2025-12-12 11:05:03 +0100

    Add block_loop function to manage app and tab blocking

--- tryingITout.py
+++ tryingITout.py
@@ -26,3 +26,22 @@ def get_installed_apps():
             apps.append(item.replace(".app",""))
 
     return sorted(apps)
+
+
+def block_loop(duration_minutes):
+    global focus_running
+    focus_running=True
+    end_time=time.time()+duration_minutes*60
+
+    while time.time()<end_time and focus_running:
+
+        for app in BLOCKLIST_APPS:
+            kill_app_by_name(app)
+
+        tabs=getCurrentChromeTabs()
+        for tab in tabs:
+            url=tab["url"]
+            if any(site in url for site in BLOCKLIST_WEBSITES):
+                closeChromeTab(url,tab["window"])
+
+    focus_running=False
